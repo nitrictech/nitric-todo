22d1a0d7e49c47c7513843ca867cd59bfd84b7e1
Ryan Cartwright <ryan.cartwright@nitric.io>
2022-03-21 10:36:07 +1100

    Remove monorepo config for nextjs

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,1 +1,12 @@
-module.exports = require("config/eslint-preset");
+module.exports = {
+  extends: ["next", "prettier"],
+  settings: {
+    next: {
+      rootDir: ["apps/*/", "packages/*/"],
+    },
+  },
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+  },
+};
